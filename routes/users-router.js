6efe545f99bfa862e4fa39805e5d24f148fb3bff
Patrick Rigg <patrickarigg@gmail.com>
2023-11-23 11:01:08 +0000

    completes endpoint for GET /api/users/:username to get a single user

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,8 +1,12 @@
-const { getAllUsers } = require("../controllers/app.controller");
+const {
+  getAllUsers,
+  getUserByUsername,
+} = require("../controllers/app.controller");
 
 const usersRouter = require("express").Router();
 
 //requests
 usersRouter.get("", getAllUsers);
+usersRouter.get("/:username",getUserByUsername)
 
 module.exports = usersRouter
