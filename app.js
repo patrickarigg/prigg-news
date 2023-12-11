01eda93f29811cf9a3dd54fbe3eb0ff9ca59742d
Patrick Rigg <patrickarigg@gmail.com>
2023-12-11 10:22:59 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const {
   handlePostgresErrors,
@@ -10,7 +11,8 @@ const { handleInvalidEndpoints } = require("./controllers/app.controller");
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //routes
 app.use("/api", apiRouter);
